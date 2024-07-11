309ca989716dd0a284d818d1d71d81951cc6ccbe
the bao <boykg1900@gmail.com>
2024-07-11 14:18:46 +0700

    update Insert, update, delete State

--- src/main/resources/static/js/state.js
+++ src/main/resources/static/js/state.js
@@ -1,30 +1,94 @@
-
-$('document').ready(function(){
+//
+//$('document').ready(function(){
+//    $('table #editButton').on('click', function(event){
+//        event.preventDefault();
+//
+//        var href = $(this).attr('href');
+//
+//        $.get(href, function(state, status){
+//            $('#idEdit').val(state.id)
+//            $('#ddlCountryEdit').val(state.countryid);
+//            $('#capitalEdit').val(state.capital);
+//             $('#codeEdit').val(state.code);
+//            $('#nameEdit').val(state.name);
+//            $('#detailsEdit').val(state.details);
+//        });
+//        $('#editModal').modal();
+//    });
+//
+//    $('.table #detailsButton').on('click', function(event){
+//    event.preventDefault();
+//    var href = $(this).attr('href');
+//    $.get(href, function(state, status){
+//        $('#idDetails').vak(state.id);
+//        $('#ddlCountryDetails').val(state.countryid);
+//        $('#capitalEdit').val(state.capital);
+//        $('#nameDetails').val(state.name);
+//        $('#lastModifiedByDetails').val(state.lastModifiedBy);
+//        $('#lastModifiedDateDetails').val(state.lastModifiedDate.substr(0,19).replace("T", " "));
+//    });
+//
+//
+//    $('table #deleteButton').on('click', function(event){
+//     event.preventDefault();
+//
+//     var href = $(this).attr('href');
+//
+//     $('#confirmDeleteButton').attr('href', href);
+//
+//     $('#deleteModal').modal();
+//      });
+//});
+//
+//
+//
+//
+$(document).ready(function(){
     $('.table .btn-primary').on('click', function(event){
         event.preventDefault();
 
         var href = $(this).attr('href');
 
         $.get(href, function(state, status){
-            $('#idEdit').val(state.id)
-            $('#ddlCountryEdit').val(state.description);
+            $('#idEdit').val(state.id);
+            $('#ddlCountryEdit').val(state.countryid);
+            $('#capitalEdit').val(state.capital);
+            $('#codeEdit').val(state.code);
             $('#nameEdit').val(state.name);
             $('#detailsEdit').val(state.details);
         });
+
         $('#editModal').modal();
     });
 
-    $('table #deleteButton').on('click', function(event){
-     event.preventDefault();
-
-     var href = $(this).attr('href');
-
-     $('#confirmDeleteButton').attr('href', href);
-
-     $('#deleteModal').modal();
-      });
 });
+$(document).ready(function(){
+    $('.table .btn-success').on('click', function(event){
+        event.preventDefault();
 
+        var href = $(this).attr('href');
 
+        $.get(href, function(state, status){
+            $('#idDetails').val(state.id);
+            $('#ddlCountryDetails').val(state.countryid);
+            $('#capitalDetails').val(state.capital);
+            $('#nameDetails').val(state.name);
+            $('#lastModifiedByDetails').val(state.lastModifiedBy);
+            $('#lastModifiedDateDetails').val(state.lastModifiedDate.substr(0, 19).replace("T", " "));
+        });
+
+        $('#detailsModal').modal();
+    });
+
+    $('table #deleteButton').on('click', function(event){
+         event.preventDefault();
+
+         var href = $(this).attr('href');
+
+         $('#confirmDeleteButton').attr('href', href);
+
+         $('#deleteModal').modal();
+    });
+});
 
 
