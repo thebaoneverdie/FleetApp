a8a90af046bb9af69cbf4838216a2ca2bdf81b5d
the bao <boykg1900@gmail.com>
2024-07-15 10:05:41 +0700

    Insert, update, delete Location and fix link html location to locations

--- src/main/java/com/thebao/fleetapp/controllers/LocationController.java
+++ src/main/java/com/thebao/fleetapp/controllers/LocationController.java
@@ -1,13 +1,55 @@
 package com.thebao.fleetapp.controllers;
 
+import com.thebao.fleetapp.models.Location;
+import com.thebao.fleetapp.services.CountryService;
+import com.thebao.fleetapp.services.LocationService;
+import com.thebao.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class LocationController {
-    @GetMapping("/location")
-    public String getLocations(){
+    @Autowired private StateService stateService;
+    @Autowired private CountryService countryService;
+
+    @Autowired private LocationService locationService;
+
+    //Get All States
+    @GetMapping("locations")
+    public String findAll(Model model){
+        model.addAttribute("states", stateService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+        model.addAttribute("locations", locationService.findAll());
         return "location";
     }
 
+    @RequestMapping("locations/findById")
+    @ResponseBody
+    public Optional<Location> findById(Integer id)
+    {
+        return locationService.findById(id);
+    }
+
+    //Add Location
+    @PostMapping(value="locations/addNew")
+    public String addNew(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value="locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value="locations/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        locationService.deleteById(id);
+        return "redirect:/locations";
+    }
 }
--- src/main/java/com/thebao/fleetapp/services/LocationService.java
+++ src/main/java/com/thebao/fleetapp/services/LocationService.java
@@ -1,7 +1,44 @@
 package com.thebao.fleetapp.services;
 
+import com.thebao.fleetapp.models.Location;
+import com.thebao.fleetapp.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public List<Location> getLocations(){
+        return locationRepository.findAll();
+    }
+
+    public List<Location> findAll() {
+        return locationRepository.findAll();
+    }
+
+    //Save new country
+    public void save(Location location){
+        locationRepository.save(location);
+    }
+
+
+
+    //Get by id
+    public Optional<Location> findById(int id){
+        return locationRepository.findById(id);
+    }
+
+    public void update(Location location) {
+        locationRepository.save(location);
+    }
+
+    public void deleteById(Integer id){
+        locationRepository.deleteById(id);
+    }
+
 }
