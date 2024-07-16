5c8fc80bf87a21638c7072b76d67309c8e881816
the bao <boykg1900@gmail.com>
2024-07-16 14:01:05 +0700

    Insert - update - delete Client model

--- src/main/java/com/thebao/fleetapp/controllers/ClientController.java
+++ src/main/java/com/thebao/fleetapp/controllers/ClientController.java
@@ -1,12 +1,55 @@
 package com.thebao.fleetapp.controllers;
 
+import com.thebao.fleetapp.models.Client;
+import com.thebao.fleetapp.services.CountryService;
+import com.thebao.fleetapp.services.ClientService;
+import com.thebao.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ClientController {
-    @GetMapping("/clients")
-    public String getClients(){
+    @Autowired private ClientService clientService;
+    @Autowired private CountryService countryService;
+
+    @Autowired private StateService stateService;
+
+    //Get All Clients
+    @GetMapping("clients")
+    public String findAll(Model model){
+        model.addAttribute("clients", clientService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+        model.addAttribute("states", stateService.findAll());
         return "client";
+    }
+
+    @RequestMapping("clients/findById")
+    @ResponseBody
+    public Optional<Client> findById(Integer id)
+    {
+        return clientService.findById(id);
+    }
+
+    //Add Client
+    @PostMapping(value="clients/addNew")
+    public String addNew(Client client) {
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping(value="clients/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Client client) {
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping(value="clients/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        clientService.deleteById(id);
+        return "redirect:/clients";
     }
 }
--- src/main/java/com/thebao/fleetapp/services/ClientService.java
+++ src/main/java/com/thebao/fleetapp/services/ClientService.java
@@ -1,7 +1,43 @@
 package com.thebao.fleetapp.services;
 
+import com.thebao.fleetapp.models.Client;
+import com.thebao.fleetapp.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
+
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    //Save new country
+    public void save(Client client){
+        clientRepository.save(client);
+    }
+
+
+
+    //Get by id
+    public Optional<Client> findById(int id){
+        return clientRepository.findById(id);
+    }
+
+    public void update(Client client) {
+        clientRepository.save(client);
+    }
+
+    public void deleteById(Integer id){
+        clientRepository.deleteById(id);
+    }
 }
