7e109d3714e0021cd6d90f6bfbe66e95bf0afc93
the bao <boykg1900@gmail.com>
2024-07-17 13:38:33 +0700

    Insert - update - delete Supplier model

--- src/main/java/com/thebao/fleetapp/controllers/SupplierController.java
+++ src/main/java/com/thebao/fleetapp/controllers/SupplierController.java
@@ -1,13 +1,56 @@
 package com.thebao.fleetapp.controllers;
 
+import com.thebao.fleetapp.models.Supplier;
+import com.thebao.fleetapp.services.SupplierService;
+import com.thebao.fleetapp.services.CountryService;
+import com.thebao.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class SupplierController {
-    @GetMapping("/suppliers")
-    public String getSupplier(){
+    @Autowired
+    private SupplierService supplierService;
+    @Autowired private CountryService countryService;
+    @Autowired private StateService stateService;
+
+    //Get All Suppliers
+    @GetMapping("suppliers")
+    public String findAll(Model model){
+        model.addAttribute("suppliers", supplierService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+        model.addAttribute("states", stateService.findAll());
         return "supplier";
+    }
+
+    @RequestMapping("suppliers/findById")
+    @ResponseBody
+    public Optional<Supplier> findById(Integer id)
+    {
+        return supplierService.findById(id);
+    }
+
+    //Add Supplier
+    @PostMapping(value="suppliers/addNew")
+    public String addNew(Supplier supplier) {
+        supplierService.save(supplier);
+        return "redirect:/suppliers";
+    }
+
+    @RequestMapping(value="suppliers/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Supplier supplier) {
+        supplierService.save(supplier);
+        return "redirect:/suppliers";
+    }
+
+    @RequestMapping(value="suppliers/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        supplierService.deleteById(id);
+        return "redirect:/suppliers";
     }
 
 }
--- src/main/java/com/thebao/fleetapp/services/SupplierService.java
+++ src/main/java/com/thebao/fleetapp/services/SupplierService.java
@@ -1,7 +1,43 @@
 package com.thebao.fleetapp.services;
 
+import com.thebao.fleetapp.models.Supplier;
+import com.thebao.fleetapp.repositories.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SupplierService {
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public List<Supplier> getSuppliers(){
+        return supplierRepository.findAll();
+    }
+
+    public List<Supplier> findAll() {
+        return supplierRepository.findAll();
+    }
+
+    //Save new country
+    public void save(Supplier supplier){
+        supplierRepository.save(supplier);
+    }
+
+
+
+    //Get by id
+    public Optional<Supplier> findById(int id){
+        return supplierRepository.findById(id);
+    }
+
+    public void update(Supplier supplier) {
+        supplierRepository.save(supplier);
+    }
+
+    public void deleteById(Integer id){
+        supplierRepository.deleteById(id);
+    }
 }
