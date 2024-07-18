4d0a34435e56e0fd4ca8c045415373ca3ded0ddb
the bao <boykg1900@gmail.com>
2024-07-18 08:43:22 +0700

    Insert - update - delete Contact model

--- src/main/java/com/thebao/fleetapp/controllers/ContactController.java
+++ src/main/java/com/thebao/fleetapp/controllers/ContactController.java
@@ -1,13 +1,55 @@
 package com.thebao.fleetapp.controllers;
 
+import com.thebao.fleetapp.models.Contact;
+import com.thebao.fleetapp.services.ContactService;
+import com.thebao.fleetapp.services.CountryService;
+import com.thebao.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ContactController {
-    @GetMapping("/contacts")
-    public String getContacts(){
+    @Autowired private ContactService contactService;
+    @Autowired private CountryService countryService;
+    @Autowired private StateService stateService;
+
+    //Get All Contacts
+    @GetMapping("contacts")
+    public String findAll(Model model){
+        model.addAttribute("contacts", contactService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+        model.addAttribute("states", stateService.findAll());
         return "contact";
+    }
+
+    @RequestMapping("contacts/findById")
+    @ResponseBody
+    public Optional<Contact> findById(Integer id)
+    {
+        return contactService.findById(id);
+    }
+
+    //Add Contact
+    @PostMapping(value="contacts/addNew")
+    public String addNew(Contact contact) {
+        contactService.save(contact);
+        return "redirect:/contacts";
+    }
+
+    @RequestMapping(value="contacts/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Contact contact) {
+        contactService.save(contact);
+        return "redirect:/contacts";
+    }
+
+    @RequestMapping(value="contacts/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        contactService.deleteById(id);
+        return "redirect:/contacts";
     }
 
 }
--- src/main/java/com/thebao/fleetapp/services/ContactService.java
+++ src/main/java/com/thebao/fleetapp/services/ContactService.java
@@ -1,7 +1,43 @@
 package com.thebao.fleetapp.services;
 
+import com.thebao.fleetapp.models.Contact;
+import com.thebao.fleetapp.repositories.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public List<Contact> getContacts(){
+        return contactRepository.findAll();
+    }
+
+    public List<Contact> findAll() {
+        return contactRepository.findAll();
+    }
+
+    //Save new country
+    public void save(Contact contact){
+        contactRepository.save(contact);
+    }
+
+
+
+    //Get by id
+    public Optional<Contact> findById(int id){
+        return contactRepository.findById(id);
+    }
+
+    public void update(Contact contact) {
+        contactRepository.save(contact);
+    }
+
+    public void deleteById(Integer id){
+        contactRepository.deleteById(id);
+    }
 }
