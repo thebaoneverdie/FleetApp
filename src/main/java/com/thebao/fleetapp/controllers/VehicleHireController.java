477ccabf6326ddd1c21f10e6a36e72708337402f
the bao <boykg1900@gmail.com>
2024-07-22 10:15:51 +0700

    insert - update - delete VehicleHire

--- src/main/java/com/thebao/fleetapp/controllers/VehicleHireController.java
+++ src/main/java/com/thebao/fleetapp/controllers/VehicleHireController.java
@@ -1,13 +1,60 @@
 package com.thebao.fleetapp.controllers;
 
+import com.thebao.fleetapp.models.VehicleHire;
+import com.thebao.fleetapp.services.ClientService;
+import com.thebao.fleetapp.services.LocationService;
+import com.thebao.fleetapp.services.VehicleHireService;
+import com.thebao.fleetapp.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class VehicleHireController {
-    @GetMapping("/vehicleHires")
-    public String getVehicleHires(){
-        return "vehiclehire";
+    @Autowired
+    private VehicleHireService vehicleHireService;
+    @Autowired private ClientService clientService;
+    @Autowired private LocationService locationService;
+    @Autowired private VehicleService vehicleService;
+
+    //Get All VehicleHires
+    @GetMapping("vehicleHires")
+    public String findAll(Model model){
+        model.addAttribute("vehicleHires", vehicleHireService.findAll());
+        model.addAttribute("clients", clientService.findAll());
+        model.addAttribute("locations", locationService.findAll());
+        model.addAttribute("vehicles", vehicleService.findAll());
+
+        return "vehicleHire";
+    }
+
+    @RequestMapping("vehicleHires/findById")
+    @ResponseBody
+    public Optional<VehicleHire> findById(Integer id)
+    {
+        return vehicleHireService.findById(id);
+    }
+
+    //Add VehicleHire
+    @PostMapping(value="vehicleHires/addNew")
+    public String addNew(VehicleHire vehicleHire) {
+        vehicleHireService.save(vehicleHire);
+        return "redirect:/vehicleHires";
+    }
+
+    @RequestMapping(value="vehicleHires/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(VehicleHire vehicleHire) {
+        vehicleHireService.save(vehicleHire);
+        return "redirect:/vehicleHires";
+    }
+
+    @RequestMapping(value="vehicleHires/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        vehicleHireService.delete(id);
+        return "redirect:/vehicleHires";
     }
 
 }
--- src/main/java/com/thebao/fleetapp/services/VehicleHireService.java
+++ src/main/java/com/thebao/fleetapp/services/VehicleHireService.java
@@ -1,7 +1,36 @@
 package com.thebao.fleetapp.services;
 
+import com.thebao.fleetapp.models.VehicleHire;
+import com.thebao.fleetapp.repositories.VehicleHireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleHireService {
+    @Autowired
+    private VehicleHireRepository vehicleHireRepository;
+
+    //Get All VehicleHires
+    public List<VehicleHire> findAll(){
+        return vehicleHireRepository.findAll();
+    }
+
+    //Get VehicleHire By Id
+    public Optional<VehicleHire> findById(int id) {
+        return vehicleHireRepository.findById(id);
+    }
+
+    //Delete VehicleHire
+    public void delete(int id) {
+        vehicleHireRepository.deleteById(id);
+    }
+
+    //Update VehicleHire
+    public void save(VehicleHire vehicleHire) {
+        vehicleHireRepository.save(vehicleHire);
+    }
+
 }
