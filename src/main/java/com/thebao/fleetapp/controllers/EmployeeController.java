f744e81e08f59ce0ca9ce4e7552e6ae0f05404d4
the bao <boykg1900@gmail.com>
2024-07-24 09:11:05 +0700

    insert - update - delete Employee

--- src/main/java/com/thebao/fleetapp/controllers/EmployeeController.java
+++ src/main/java/com/thebao/fleetapp/controllers/EmployeeController.java
@@ -1,13 +1,58 @@
 package com.thebao.fleetapp.controllers;
 
+import com.thebao.fleetapp.models.Employee;
+import com.thebao.fleetapp.services.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class EmployeeController {
-    @GetMapping("/employees")
-    public String getEmployees(){
+    @Autowired private EmployeeService employeeService;
+    @Autowired private StateService stateService;
+    @Autowired private JobTitleService jobTitleService;
+    @Autowired private EmployeeTypeService employeeTypeService;
+    @Autowired private CountryService countryService;
+
+    //Get All Employees
+    @GetMapping("employees")
+    public String findAll(Model model){
+        model.addAttribute("employees", employeeService.findAll());
+        model.addAttribute("employeeTypes", employeeTypeService.findAll());
+        model.addAttribute("states", stateService.findAll());
+        model.addAttribute("jobTitles", jobTitleService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+
         return "employee";
+    }
+
+    @RequestMapping("employees/findById")
+    @ResponseBody
+    public Optional<Employee> findById(Integer id)
+    {
+        return employeeService.findById(id);
+    }
+
+    //Add Employee
+    @PostMapping(value="employees/addNew")
+    public String addNew(Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees";
+    }
+
+    @RequestMapping(value="employees/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees";
+    }
+
+    @RequestMapping(value="employees/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        employeeService.deleteById(id);
+        return "redirect:/employees";
     }
 
 }
--- src/main/java/com/thebao/fleetapp/services/EmployeeService.java
+++ src/main/java/com/thebao/fleetapp/services/EmployeeService.java
@@ -1,11 +1,14 @@
 package com.thebao.fleetapp.services;
 
 import com.thebao.fleetapp.models.Employee;
+import com.thebao.fleetapp.models.Employee;
+import com.thebao.fleetapp.repositories.EmployeeRepository;
 import com.thebao.fleetapp.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -14,6 +17,25 @@ public class EmployeeService {
 
     public List<Employee> findAll(){
         return employeeRepository.findAll();
+    }
+
+    //Save new country
+    public void save(Employee employee){
+        employeeRepository.save(employee);
+    }
+
+
+    //Get by id
+    public Optional<Employee> findById(int id){
+        return employeeRepository.findById(id);
+    }
+
+    public void update(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    public void deleteById(Integer id){
+        employeeRepository.deleteById(id);
     }
 
 }
